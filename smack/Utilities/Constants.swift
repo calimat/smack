b0b013006e0cfacf2e04b447ea2c21037fdf5cc7
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-08-08 20:27:33 -0500

    made loginVC UI

---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Ricardo Herrera Petit on 8/8/17.
+//  Copyright © 2017 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
