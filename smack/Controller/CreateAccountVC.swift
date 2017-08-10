e259ea0c2975705f3118e0aab0ed6d3c985bd9b3
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-08-09 22:07:00 -0500

    call auth request and added rounded buttons through a view

--- smack/Controller/CreateAccountVC.swift
+++ smack/Controller/CreateAccountVC.swift
@@ -10,10 +10,38 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var emailTxT: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    
+    @IBOutlet weak var userImg: UIImageView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxT.text , emailTxT.text != "" else
+        { return}
+        guard let pass = passTxt.text , passTxt.text != "" else
+        {return}
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user!")
+            }
+            
+        }
+        
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
---
+++ smack/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  smack
+//
+//  Created by Ricardo Herrera Petit on 8/9/17.
+//  Copyright © 2017 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+}
