008ba280050bf93f7679c8c763bcb9ba00858484
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-08-10 20:42:08 -0500

    Used json and managed sifty json the traditional way

--- smack/Controller/CreateAccountVC.swift
+++ smack/Controller/CreateAccountVC.swift
@@ -35,12 +35,18 @@ class CreateAccountVC: UIViewController {
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                print("registered user!")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    
+                    if success {
+                        print("logged in user!", AuthService.instance.authToken)
+                    }
+                })
             }
             
         }
         
     }
+    
     
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
