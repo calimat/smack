5e3c697bf3902aea1de61b83032de80e2268adda
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-08-20 21:39:37 -0500

    Unread channels

--- smack/Controller/ChannelVC.swift
+++ smack/Controller/ChannelVC.swift
@@ -32,6 +32,13 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 self.tableView.reloadData()
             }
         }
+        
+        SocketService.instance.getChatMessage { (newMessage) in
+            if newMessage.channelId != MessageService.instance.selectedChannel?._id && AuthService.instance.isLoggedIn {
+                MessageService.instance.unreadChannels.append(newMessage.channelId)
+                self.tableView.reloadData()
+            }
+        }
     }
   
     override func viewDidAppear(_ animated: Bool) {
@@ -91,6 +98,15 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let channel = MessageService.instance.channels[indexPath.row]
         MessageService.instance.selectedChannel = channel
+        
+        if MessageService.instance.unreadChannels.count > 0 {
+            MessageService.instance.unreadChannels = MessageService.instance.unreadChannels.filter{$0 != channel._id}
+        }
+        
+        let index = IndexPath(row: indexPath.row, section: 0)
+        tableView.reloadRows(at: [index], with: .none)
+        tableView.selectRow(at: index, animated: false, scrollPosition: .none)
+                
         NotificationCenter.default.post(name: NOTIF_CHANNELS_SLECTED, object: nil)        
         self.revealViewController().revealToggle(animated: true)
     }
